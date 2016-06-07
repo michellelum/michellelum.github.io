2a76a58c1d68a8e71b29949b4b70a27c1e8b675f
michellelum <mlum@sea-c02j94fadkq4.local>
2016-06-06 20:43:10 -0700

    copy from teamlum.com

---
+++ PUBLICATIONS_files/PUBLICATIONS.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20120702
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('PUBLICATIONS_files/PUBLICATIONSMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ TEACHING_files/TEACHING.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.3 local-build-20120702
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('TEACHING_files/TEACHINGMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
